620e12404fef12f79fea506b96a3c18749f5127b
paul-madut <paul.madut@mths.ca>
2023-02-02 20:25:25 -0500

    Appends input to array
    Appends input to array and prints it out to console

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -2,18 +2,48 @@ import './App.css';
 import { useState} from 'react';
 
 function App() {
+
+  function addItem(){
+    //Random id setter for each item
+    let randomId = Math.floor(Math.random()*1000);
+
+    while(usedIds.includes(randomId)){
+      randomId = Math.floor(Math.random()*1000);
+    }
+
+    const listItem = {
+      id: randomId,
+      value: item
+    }
+    setList(oldList => [...oldList,listItem]);
+    console.log(list);
+    setItem("")
+  }
+
+  const [item,setItem] = useState("");
+  const [list,setList] = useState([]);
+  const usedIds = [];
+
+
   return (
     <>
     <h1 className='title'>Paul's One Thing</h1>
-    <MyButton >Add Todo</MyButton>
-    <input type="text" placeholder='What needs to get done?'></input>
+    <MyButton onClick={()=>addItem(item)}>Add Todo</MyButton>
+    <InputField onChange = {(e)=> setItem(e.target.value)} value = {item} type="text" placeholder='What needs to get done?'></InputField>
     <MyButton>Clear</MyButton>
     
     </>
   );
 }
 
-
+function InputField(props){
+  return(
+  <input {...props}
+    >
+  
+  </input>
+  )
+}
 
 function MyButton(props){
   return(
@@ -22,6 +52,8 @@ function MyButton(props){
     </>
   )
 }
+
+
 
 
 export default App;
